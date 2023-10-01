536d4e4e8576e803e5479e96a8415a945492e1fa
sgohlke <gohlke.stefan@yahoo.de>
2023-10-01 15:59:31 +0200

    Make PlayerDataStore async

--- src/player/PlayerDataStore.ts
+++ src/player/PlayerDataStore.ts
@@ -1,12 +1,12 @@
 import { Player, PlayerAccount } from '../index.ts'
 
 export interface PlayerDataStore {
-   addPlayerAccount(playerAccount: PlayerAccount): void
-   createPlayer(player: Player): string
-   doesPlayerExist(userName: string): boolean
-   getAccessTokenForPlayer(playerId: string): string | undefined
-   getPlayer(playerId: string | undefined): Player | undefined
-   getPlayerAccount(playerId: string): PlayerAccount | undefined
-   getPlayerAccountForName(username: string): PlayerAccount | undefined
-   setPlayerAccessToken(playerId: string, accessToken: string): void
+   addPlayerAccount(playerAccount: PlayerAccount): Promise<void>
+   createPlayer(player: Player): Promise<string>
+   doesPlayerExist(userName: string): Promise<boolean>
+   getAccessTokenForPlayer(playerId: string): Promise<string | undefined>
+   getPlayer(playerId: string | undefined): Promise<Player | undefined>
+   getPlayerAccount(playerId: string): Promise<PlayerAccount | undefined>
+   getPlayerAccountForName(username: string): Promise<PlayerAccount | undefined>
+   setPlayerAccessToken(playerId: string, accessToken: string): Promise<void>
 }
